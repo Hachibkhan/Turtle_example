16b3b5e147e558fd3ea14070fcc716028c70b0e2
Hachibkhan <hachib.khan19@gmail.com>
2019-05-04 21:44:55 +0600

    Turtle.py

---
+++ first.py
@@ -0,0 +1,17 @@
+import turtle
+
+turtle.shape("turtle")
+
+turtle.speed(2)
+
+turtle.color("green")
+
+for i in range(4):
+
+    turtle.forward(100)
+
+    turtle.left(90)
+
+turtle.exitonclick()
+
+
